fef2ec393f82ceab216e1b9217777bf90204ae5e
John Pelliccia <jpelliccia@gmail.com>
2019-07-04 17:17:08 -0400

    Week5 Code Labs

---
+++ android-codelabs-101-Shrine/Code/java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
@@ -0,0 +1,54 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+/**
+ * Fragment representing the login screen for Shrine.
+ */
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.shr_login_fragment, container, false);
+
+        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        MaterialButton nextButton = view.findViewById(R.id.next_button);
+
+        // Set sets error fpr less then 8 characters.
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(getString(R.string.shr_error_password));
+                } else {
+                    passwordTextInput.setError(null); // Clear the error
+                    ((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false); // Navigates to the next Fragment
+                }
+            }
+        });
+
+        return view;
+    }
+
+    // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+
+    private boolean isPasswordValid(@Nullable Editable text) {
+        return text != null && text.length() >= 8;
+    }
+
+
+}
